9247b464413279c5e39963e333dd29ed7315eee2
Pahrurozi <61068212+pahrurozidev@users.noreply.github.com>
2022-11-09 13:18:24 +0800

    Fix all bug on production

--- src/components/navbar/NavItem.jsx
+++ src/components/navbar/NavItem.jsx
@@ -4,20 +4,20 @@ export default function NavItem({ search }) {
     return (
         <Fragment>
             {search &&
-                <li className="list-group-item me-4">
-                    <a className="nav-link py-2" href="/cars">Search Car</a>
+                <li className="list-group-item me-4 bg-transparent border-0">
+                    <a className="nav-link py-2" href="/cars" style={{ color: "black" }}>Search Car</a>
                 </li>}
-            <li className="list-group-item me-4">
-                <a className="nav-link py-2" href="#services">Our Services</a>
+            <li className="list-group-item me-4 bg-transparent border-0">
+                <a className="nav-link py-2" href="#services" style={{ color: "black" }}>Our Services</a>
             </li>
-            <li className="list-group-item me-4">
-                <a className="nav-link py-2" href="#whyus">Why Us</a>
+            <li className="list-group-item me-4 bg-transparent border-0">
+                <a className="nav-link py-2" href="#whyus" style={{ color: "black" }}>Why Us</a>
             </li>
-            <li className="list-group-item me-4">
-                <a className="nav-link py-2" href="#testimonial">Testimonial</a>
+            <li className="list-group-item me-4 bg-transparent border-0">
+                <a className="nav-link py-2" href="#testimonial" style={{ color: "black" }}>Testimonial</a>
             </li>
-            <li className="list-group-item me-4">
-                <a className="nav-link py-2" href="#faq">FAQ</a>
+            <li className="list-group-item me-4 bg-transparent border-0">
+                <a className="nav-link py-2" href="#faq" style={{ color: "black" }}>FAQ</a>
             </li>
             <li className="list-group-item list-group-item--modifier">
                 <a className="nav-link text-white px-3 py-2" href="#">Register</a>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import Routes from './routes/Routes'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './redux/reducer/globalReducer';
 
 // styling
-import "./assets/bootstrap-5.2.0-dist/css/bootstrap.min.css";
-import "./assets/bootstrap-5.2.0-dist/js/bootstrap.min.js";
-import "./assets/css/style.css";
-import rootReducer from './redux/reducer/globalReducer';
+// import "./assets/bootstrap-5.2.0-dist/css/bootstrap.min.css";
+// import "./assets/bootstrap-5.2.0-dist/js/bootstrap.min.js";
+// import "./assets/css/style.css";
 
 const storeRedux = createStore(rootReducer);
 
